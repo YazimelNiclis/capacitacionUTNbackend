69964272477a0d50802e8c57834db1788b3b623a
yazimel.niclis <yazimelniclis1995@gmail.com>
2022-11-26 13:11:17 -0300

    cors

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
-}
+app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 
 //ENDPOINTS
 app.get("/api", (req, res) => {
